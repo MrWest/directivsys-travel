e47d6ee88646198a07681e8c7901eabacea2d342
MrWest <kidwest87@gmail.com>
2026-07-19 15:00:54 -0300

    feat: apiKey

--- components/DirectivSysWrapper.tsx
+++ components/DirectivSysWrapper.tsx
@@ -5,7 +5,7 @@ import { DirectivSysProvider } from '@directivsys/react-sdk';
 export function DirectivSysWrapper({ children }: { children: ReactNode }) {
   return (
     <DirectivSysProvider
-      apiKey="dsk_bf71c83699c84f26a2bc5f7ce6b973a3"
+      apiKey="dsk_ac71a83699c84f26a2bc5f7ce6b953d4"
       config={{
         baseURL: 'https://staging-api.directivsys.com',
         timeout: 30000,
